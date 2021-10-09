616d4869854d1913c8a18a9bd8f59684e71a2834
kartikmadan <kartikmadan26@gmail.com>
2021-10-09 22:28:13 +0530

    updated counter

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 9, 9, 16,00)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2021, 9, 10, 16,30)}); // year, month, date, hour
         });
 });		
 
